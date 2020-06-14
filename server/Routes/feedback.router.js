9488a72b989492ba8831d1071a8fd281aaa62d23
Nailah Johnson <nailahjohnson@Nailahs-Air.zyxel.com>
2020-06-14 12:58:37 -0500

    added express router for feedback router

--- server/Routes/feedback.router.js
+++ server/Routes/feedback.router.js
@@ -3,7 +3,19 @@ const router = express.Router();
 
 const pool = require('../modules/pool')
 
+router.post('/', (req, res) => {
+    console.log(req.body);
 
+    let queryText = `INSERT INTO "feedback" ("feeling", "understanding", "support", "comments")`
 
+    pool.query(queryText, [req.body.feeling, req.body.understanding, req.body.support, req.body.comments])
+        .then((result) => {
+            console.log('CREATED!');
+            res.sendStatus(201);
+        }).catch((err) => {
+            console.log('error in POST', err);
+            res.sendStatus(500);
+        })
+});
 
 module.exports = router;
